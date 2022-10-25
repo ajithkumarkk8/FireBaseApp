9f298a171ec8f3d180262c9602344726ce6947a7
ajithkumarkk8 <ajithkumark111095@gmail.com>
2022-10-25 10:55:31 +0530

    ajnj

---
+++ dub.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Provider as PaperProvider} from 'react-native-paper';
+import App from './src';
+import {theme} from './src/core/theme';
+const Main = () => (
+  <PaperProvider theme={theme}>
+    <App />
+  </PaperProvider>
+);
+
+export default Main;
